ad2b907b6861fef5f452c35bffb91fe6e6827e6f
osama maree <osamamarei2001@gmail.com>
2023-09-23 21:17:49 +0300

    Add Edit For Code Review

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,33 @@
-const calc = (...arg) => {
+const calculator = (...arg) => {
   if (arg.length < 3 || !(arg.length % 2)) {
     throw new Error("Invalid number of inputs");
   }
   // Ignore vale more than 1000
-    for (let i = 0; i < arg.length; i += 2) {
-      if (arg[i] > 1000 && i == 0) {
-        arg.splice(i, 2);
-        i -= 2;
-      } else if (arg[i] > 1000) {
-        arg.splice(i - 1, 2);
-        i -= 2;
-      }
+  for (let i = 0; i < arg.length; i += 2) {
+    if (arg[i] > 1000 && i == 0) {
+      arg.splice(i, 2);
+      i -= 2;
+    } else if (arg[i] > 1000) {
+      arg.splice(i - 1, 2);
+      i -= 2;
     }
+  }
+  let skip = false;
+  arg = arg.filter((value, index) => {
+    if (skip) {
+      skip = false;
+      return false;
+    }
+    if (value > 1000) {
+      if (index === 0) {
+        skip = true;
+      } else {
+        arg.splice(index - 1, 2);
+      }
+      return false;
+    }
+    return true;
+  });
   const equ = [];
   for (let i = 1; i < arg.length; i += 2) {
     if (arg[i] !== "-" && arg[i] !== "+" && arg[i] !== "*" && arg[i] !== "/") {
@@ -29,10 +45,18 @@ const calc = (...arg) => {
   }
   if (equ.length == 0) return arg[arg.length - 1];
   if (typeof equ[equ.length - 1] !== "number") equ.push(arg[arg.length - 1]);
-  for (let i = 1; i < equ.length; i += 2) {
-    equ[i + 1] = evaluate(equ[i - 1], equ[i + 1], equ[i]);
-  }
-  return equ[equ.length - 1];
+
+  const result = equ.reduce(
+    (accumulator, currentValue, currentIndex, array) => {
+      if (currentIndex % 2 === 0 && currentValue !== 0) {
+        const operator = array[currentIndex - 1];
+        return evaluate(accumulator, currentValue, operator);
+      }
+      return accumulator;
+    }
+  );
+
+  return result;
 };
 
 function evaluate(a, b, operation) {
@@ -54,4 +78,4 @@ function evaluate(a, b, operation) {
     }
   }
 }
-module.exports = calc;
+module.exports = calculator;
